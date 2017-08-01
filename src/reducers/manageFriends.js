9bcceb78eb49cc149a226451ffea0fdc0f41536e
Jan Phillip Carreon <janphillipcarreon@yahoo.com>
2017-08-01 16:18:01 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,19 @@
-export function manageFriends(state, action){
+export function manageFriends(state = {
+  friends:[],
+}, action){
+  switch(action.type){
+    case "ADD_FRIEND":
+     state = {
+       friends:[...state.friends, action.friend]
+     }
+     break;
+    case "REMOVE_FRIEND":
+      const friends = state.friends.filter((friend)=>friend.id !== action.id)
+      state ={
+        friends:friends
+      }
+    break;
+  }
+
+  return state;
 }
